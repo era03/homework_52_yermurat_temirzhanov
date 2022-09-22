3285fefbbd1017297ca98242e9e3cbbb64f54ec4
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-09-23 00:03:30 +0600

    Добавлена модель ToDo

--- to_do_app/webapp/models.py
+++ to_do_app/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    description = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.TextField(max_length=200, null=False, blank=False, default='new', verbose_name='Статус')
+    deadline = models.DateTimeField(verbose_name='Дата выполнения')
+
+
+    def __str__(self) -> str:
+        return f"{self.description} - {self.status} - {self.deadline}"
